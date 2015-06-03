f54cc3dbc9d72d7f1497d453d1f6beb4dec21672
jonsindreu <dreubarik@hotmail.com>
2015-06-03 18:03:50 +0100

    Update api.php

--- api.php
+++ api.php
@@ -14,8 +14,39 @@ function fetchArticleList(){
     
 }
 
-function cleanArticle($article){
-    
+function CleanArticle($ArticleList) {
+ 
+//We parse the dirty article list 
+$ArticleString = file_get_contents($ArticleList);
+$ArticleParsed = json_decode($ArticleString, true);
+$Articles=$ArticleParsed["$ArticleParsed"][0]["Body"];
+
+//Array of empty strings where cleaned articles are going to go
+$CleanArticles = array_fill(0,sizeof($Articles),"");
+
+foreach($Articles as $index => $ArticlesAux) {
+	
+	$i=0
+	
+	//Each article starts empty
+	$CleanArt = "";
+	
+	//We fill each article	
+	foreach($ArticlesAux as $index2 => $Art) {
+		
+		//After checking we ended on a full sentence, we only want the first 500 characters or so of each article
+		if (substr($CleanArt, -1) != "." And strlen($CleanArt < 500)) {
+			$CleanArt = $CleanArt . $Art[0]["Items"][0]["Value"];
+		}
+	}
+	
+	//We insert the full article into the right place in the list
+	$CleanArticles[i] = $CleanArticles[i] . $CleanArt;	
+}
+
+//We return an array of cleaned articles
+return $CleanArticles;
+
 }
 
 function fetchArticle($ref){
