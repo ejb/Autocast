19218e225990929701d456b5e4ecaba71c8df3dc
ejb <mail@elliotbentley.com>
2015-06-03 16:36:45 +0100

    Basic structure

--- api.php
+++ api.php
@@ -1,5 +1,25 @@
 <?php
     
 $factiva_key = getenv( 'FACTIVA_KEY' );
+$articleRefs = fetchArticleList();
+$articles = [];
+foreach ($articleRefs as $index => $article) {
+    cleanArticle( fetchArticle( $article['ref'] ) );
+    array_push( $articles, $article );
+}
+echo json_encode( $articles );
+
+
+function fetchArticleList(){
     
+}
+
+function cleanArticle($article){
+    
+}
+
+function fetchArticle($ref){
+    
+}
+
 ?>
