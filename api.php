214bbe8141becee225b6afc1b193211b472f0dab
Charles Forelle <cforelle@gmail.com>
2015-06-04 10:19:13 +0100

    added fetchArticleList()

--- api.php
+++ api.php
@@ -11,6 +11,29 @@ echo json_encode( $articles );
 
 
 function fetchArticleList(){
+	global $factiva_key;
+	$arefs = [];
+	$days = '1';
+	$search = 'sc%3Awsjo';
+
+	$url = "http://api.dowjones.com/api/public/2.0/content/headlines/json?EncryptedToken=$factiva_key&DaysRange=$days&SearchString=$search";
+
+	$l = json_decode( file_get_contents( $url ), TRUE );
+	$chunks = ceil ( $l["TotalRecords"] / 100 );
+	$i = $chunks;
+
+	//handle article list > 100
+	while ($i > 0) {
+	 	$offset = (string)($chunks - $i) * 100;
+	 	$url_new = $url . "&Offset=$offset";
+	 	$a = json_decode( file_get_contents( $url_new ), TRUE );
+		foreach( $a["Headlines"] as $v) {
+		array_push( $arefs, $v["ArticleRef"] );
+	    }
+	 	$i -= 1; 
+	}
+
+	return $arefs; 
     
 }
 
